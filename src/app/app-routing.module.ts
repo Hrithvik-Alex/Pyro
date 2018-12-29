4f7a779f34f49ed61fd5f5fac41d0ac830466617
Hrithvik  Alex <halex623@Hrithviks-MacBook-Pro.local>
2018-12-29 18:59:55 -0500

    added components and routes for host page and join page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HostPageComponent } from './components/host-page/host-page.component';
+import { JoinPageComponent } from './components/join-page/join-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "host", component: HostPageComponent},
+  {path: "join", component: JoinPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { JoinPageComponent } from './components/join-page/join-page.component';
+import { HostPageComponent } from './components/host-page/host-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    JoinPageComponent,
+    HostPageComponent
   ],
   imports: [
     BrowserModule,
