1c062ff6a19d784b95d712472866a3d097c03fcc
Hrithvik  Alex <halex623@Hrithviks-MacBook-Pro.local>
2018-12-29 19:32:57 -0500

    fixed up routes for host and join page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
 import { HostPageComponent } from './components/host-page/host-page.component';
 import { JoinPageComponent } from './components/join-page/join-page.component';
 
 const routes: Routes = [
+  {path: 'home', component: HomePageComponent},
   {path: "host", component: HostPageComponent},
-  {path: "join", component: JoinPageComponent}
+  {path: "join", component: JoinPageComponent},
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
